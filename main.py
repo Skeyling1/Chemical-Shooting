6562bce126f2a606f13450f78c1bb900dced22c1
Skeyling1 <skeyling1@gmail.com>
2024-05-26 18:48:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ import pygame
 
 pygame.init()
 
+SCREEN_WIDTH = 800
+SCREEN_HIGTH = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HIGTH))
+
+pygame.display.set_caption("Игра Тир")
+icon = pygame.image.load("")
+
+
+
+
 running = True
 while running:
     pass
